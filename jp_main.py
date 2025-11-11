1ece5047018abc0d95404c768348db1fa2ea1457
beggarprince <beatyouonetime@gmail.com>
2025-11-11 13:04:50 -0600

    Gets stats, gets correlation, cleans data, basic filtering and extract, and has a visual. Has not been presented on Wednesday because it is not Wednesday. 6/7

---
+++ jp_main.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+v = "vgsales.csv"
+
+def main():
+    df = pd.read_csv(v)
+    
+    clean_df = df.dropna()
+    jp_sales_groupby_genre(clean_df)
+    corr_jp_sales_to_other(clean_df)
+
+
+def corr_jp_sales_to_other(df):
+    correlation = df['Other_Sales'].corr(df['JP_Sales'])
+    print("=" * 70)
+    print(f"Correlation between Other_Sales and JP_Sales: {correlation:.4f}")
+
+
+def jp_sales_groupby_genre(df):
+    jp_sales_by_genre = df.groupby('Genre')['JP_Sales'].agg([
+        ('Total_Sales', 'sum'),
+        ('Average_Sales', 'mean'),
+        ('Number_of_Games', 'count'),
+        ('Max_Sales', 'max'),
+        ('Median_Sales', 'median')
+    ]).sort_values('Total_Sales', ascending=False).round(5)
+
+    print("Japanese Sales by Genre:")
+    print("=" * 70)
+    print(jp_sales_by_genre)
+    print("=" * 70)
+
+    jp_sales_by_genre['Percentage'] = (jp_sales_by_genre['Total_Sales']/ jp_sales_by_genre['Total_Sales'].sum() *100)
+    
+    
+    print("Genre preferences (% of total JP sales):")
+    print(jp_sales_by_genre[['Total_Sales', 'Percentage']].round(2))
+
+    print("Plotting data")
+    plt.figure(figsize=(12,6))
+    plt.bar(jp_sales_by_genre.index, jp_sales_by_genre['Total_Sales'])    
+
+    plt.xlabel('Genre')
+    plt.ylabel('Total JP Sales (in millions)')
+    plt.title('Total Japan Sales by Genre')
+
+    plt.show()
+
+
+main()
